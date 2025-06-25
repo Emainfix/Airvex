7e43111e5672076705c11ae50d8474daa45a6779
Emanuel Villegas <fevillegas.dev@gmail.com>
2025-06-25 15:43:34 -0300

    Estoy intentando hacer una clase que me guarde el producto y un bucle que me ayude a verificar si está en el carrito

--- index.js
+++ index.js
@@ -1,8 +1,27 @@
-const agregar = document.getElementById("boton");
+class Producto {
+  constructor(nombre, precio, cantidad) {
+    this.cantidad = cantidad;
+    this.nombre = nombre;
+    this.precio = precio;
+  }
+}
+
+const aspiradora1 = new Producto(1, "S10 Plus", 200);
+
+const agregar = document.getElementById("boton-aspiradora1");
 
 let carrito = [];
 
 agregar.onclick = function () {
-  carrito[0] = "entro";
+  for (let i = 0; i < carrito.length; i++) {
+    for (let k = 0; k < carrito.length; k++) {
+      if (i != k && carrito[i] == carrito[k]) {
+        aspiradora1.cantidad++;
+      } else {
+        carrito.push(aspiradora1);
+      }
+    }
+  }
+
   console.log(carrito);
 };
