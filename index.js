f48ba8670303b9bb38b899b11eca28c5c3f30353
Emanuel Villegas <fevillegas.dev@gmail.com>
2025-06-25 16:50:40 -0300

    Pude lograr que los productos se agreguen al array sin suplantarse y sumando cantidad

--- index.js
+++ index.js
@@ -13,23 +13,40 @@ const aspiradora1 = new Producto(1, 1, "S10 Plus", 200);
 
 const aspiradora1btn = document.getElementById("boton-aspiradora1");
 
-let carrito = [];
-
 aspiradora1btn.onclick = function () {
   newProducto = aspiradora1;
   addProducto(newProducto);
 };
 
+const aspiradora2 = new Producto(2, 1, "pppp", 150);
+
+const aspiradora2btn = document.getElementById("boton-aspiradora2");
+
+aspiradora2btn.onclick = function () {
+  newProducto = aspiradora2;
+  addProducto(newProducto);
+};
+
+let carrito = [];
+
 function addProducto(p) {
+  let encontrado = false;
+
   if (carrito.length < 1) {
     carrito.push(p);
   } else {
     for (let i = 0; i < carrito.length; i++) {
       if (carrito[i].id == p.id) {
-        p.cantidad++;
+        encontrado = true;
+        i = carrito.length;
       } else {
-        carrito.push(p);
+        encontrado = false;
       }
+    }
+    if (encontrado) {
+      p.cantidad++;
+    } else {
+      carrito.push(p);
     }
   }
 
