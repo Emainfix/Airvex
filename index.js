c429caf92c9233ec9239b88047f9f8e69901c059
Emanuel Villegas <fevillegas.dev@gmail.com>
2025-06-25 20:48:57 -0300

    Reduje el codigo y pude encontrar un error en donde al encontrar el producto en el listado agregaba en el html un li dentro del li existente

--- index.js
+++ index.js
@@ -16,16 +16,13 @@ const lista = document.getElementById("lista-carrito");
 
 const total = document.getElementById("total");
 
-let newProducto; // Variable para agregar un producto
-
 //Boton aspiradora1
 const aspiradora1 = new Producto(1, 1, "S10 Plus", 200);
 
 const aspiradora1btn = document.getElementById("boton-aspiradora1");
 
 aspiradora1btn.onclick = function () {
-  newProducto = aspiradora1;
-  addProducto(newProducto);
+  addProducto(aspiradora1);
 };
 
 //Boton aspiradora2
@@ -34,40 +31,36 @@ const aspiradora2 = new Producto(2, 1, "pppp", 150);
 const aspiradora2btn = document.getElementById("boton-aspiradora2");
 
 aspiradora2btn.onclick = function () {
-  newProducto = aspiradora2;
-  addProducto(newProducto);
+  addProducto(aspiradora2);
 };
 
 function addProducto(p) {
+  
   let encontrado = false;
+  let indexEncontrado = carrito.items.indexOf(p);
 
-  if (carrito.items.length < 1) {
-    carrito.items.push(p);
-    lista.innerHTML += `<li>Se agregó ${p.nombre} ${p.cantidad}</li>`;
-  } else {
-    for (let i = 0; i < carrito.items.length; i++) {
-      if (carrito.items[i].id == p.id) {
-        encontrado = true;
-        i = carrito.items.length;
-      } else {
-        encontrado = false;
-      }
-    }
-    if (encontrado) {
-      p.cantidad++;
-      lista.children[
-        carrito.items.indexOf(p)
-      ].innerHTML = `<li>Se agregó ${p.nombre} ${p.cantidad}</li>`;
-    } else {
-      carrito.items.push(p);
-      lista.innerHTML += `<li>Se agregó ${p.nombre} ${p.cantidad}</li>`;
+  for (let i = 0; i < carrito.items.length; i++) {
+    if (carrito.items[i].id == p.id) {
+      encontrado = true;
+      indexEncontrado = i;
+      break
     }
   }
+  if (encontrado) {
+    p.cantidad++;
+    lista.children[indexEncontrado].innerHTML = `<strong>${p.nombre}</strong> <strong>Costo:</strong> ${p.precio}$ <strong>Cantidad:<strong/> ${p.cantidad}`;
+  } else {
+    lista.innerHTML += `<li><strong>${p.nombre}</strong> <strong>Costo:</strong> ${p.precio}$ <strong>Cantidad:<strong/> ${p.cantidad}</li>`;
+    carrito.items.push(p);
+    console.log(carrito.items.length);
+  }
+    
+
 
   //Suma el costo al total en el carrito
   carrito.total = carrito.total + p.precio;
   console.log(carrito.items);
   console.log(carrito.total);
   total.innerHTML = `<h3> ${carrito.total} </h3>`;
-  console.log(carrito.items.indexOf(p));
+  
 }
