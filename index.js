4826268f23c7b442980ad7abb6d380082c38c63b
Emanuel Villegas <fevillegas.dev@gmail.com>
2025-06-25 21:26:09 -0300

    Agregué todos los botones, hice las clases para cada producto. Modifiqué el formato de la lista y el total

--- index.js
+++ index.js
@@ -26,12 +26,39 @@ aspiradora1btn.onclick = function () {
 };
 
 //Boton aspiradora2
-const aspiradora2 = new Producto(2, 1, "pppp", 150);
+const aspiradora2 = new Producto(2, 1, "ROPVACNIC", 150);
 
 const aspiradora2btn = document.getElementById("boton-aspiradora2");
 
 aspiradora2btn.onclick = function () {
   addProducto(aspiradora2);
+};
+
+//Boton aspiradora3
+const aspiradora3 = new Producto(3, 1, "A12 Pro", 180);
+
+const aspiradora3btn = document.getElementById("boton-aspiradora3");
+
+aspiradora3btn.onclick = function () {
+  addProducto(aspiradora3);
+};
+
+//Boton aspiradora4
+const aspiradora4 = new Producto(4, 1, "BL-20 Lidar", 350);
+
+const aspiradora4btn = document.getElementById("boton-aspiradora4");
+
+aspiradora4btn.onclick = function () {
+  addProducto(aspiradora4);
+};
+
+//Boton aspiradora5
+const aspiradora5 = new Producto(5, 1, "S10 Plus", 182);
+
+const aspiradora5btn = document.getElementById("boton-aspiradora5");
+
+aspiradora5btn.onclick = function () {
+  addProducto(aspiradora5);
 };
 
 function addProducto(p) {
@@ -40,9 +67,9 @@ function addProducto(p) {
 
   if (indexEncontrado >= 0) {
     p.cantidad++;
-    lista.children[indexEncontrado].innerHTML = `<strong>${p.nombre}</strong> <strong>Costo:</strong> ${p.precio}$ <strong>Cantidad:<strong/> ${p.cantidad}`;
+    lista.children[indexEncontrado].innerHTML = `${p.nombre} <strong>US$${p.precio}</strong> <strong>Cant.:<strong/> ${p.cantidad}`;
   } else {
-    lista.innerHTML += `<li><strong>${p.nombre}</strong> <strong>Costo:</strong> ${p.precio}$ <strong>Cantidad:<strong/> ${p.cantidad}</li>`;
+    lista.innerHTML += `<li>${p.nombre} <strong>US$${p.precio}</strong> <strong>Cant.:<strong/> ${p.cantidad}</li>`;
     carrito.items.push(p);
     console.log(carrito.items.length);
   }
@@ -53,6 +80,6 @@ function addProducto(p) {
   carrito.total = carrito.total + p.precio;
   console.log(carrito.items);
   console.log(carrito.total);
-  total.innerHTML = `<h3> ${carrito.total} </h3>`;
+  total.innerHTML = `<h3> TOTAL: <strong>US$${carrito.total}</strong> </h3>`;
   
 }
