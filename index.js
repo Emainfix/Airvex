e1267ff797af446ea85203bb28cc975fecc119d5
Emanuel Villegas <fevillegas.dev@gmail.com>
2025-06-26 18:52:46 -0300

    Hice la funcion eliminar

--- index.js
+++ index.js
@@ -17,12 +17,17 @@ const lista = document.getElementById("lista-carrito");
 const total = document.getElementById("total");
 
 //Boton aspiradora1
-const aspiradora1 = new Producto(1, 1, "S10 Plus", 200);
+const aspiradora1 = new Producto(1, 0, "S10 Plus", 200);
 
 const aspiradora1btn = document.getElementById("boton-aspiradora1");
+const aspiradora1Eliminar = document.getElementById("eliminar-aspiradora1")
 
 aspiradora1btn.onclick = function () {
   addProducto(aspiradora1);
+};
+
+aspiradora1Eliminar.onclick = function () {
+  restarProducto(aspiradora1);
 };
 
 //Boton aspiradora2
@@ -73,6 +78,7 @@ function addProducto(p) {
       <div class="px-4 border-start border-3"><strong>Cant:</strong>${p.cantidad}</div>
     </div>`;
   } else {
+    p.cantidad++;
     lista.innerHTML += `<li class="d-flex justify-content-end">
     <div class="p-2 d-flex align-items-center">
       <h4 class="px-4">${p.nombre}</h4>
@@ -84,13 +90,38 @@ function addProducto(p) {
     console.log(carrito.items.length);
   }
 
-  
-
-
   //Suma el costo al total en el carrito
-  carrito.total = carrito.total + p.precio;
   console.log(carrito.items);
   console.log(carrito.total);
+  carrito.total = carrito.total + p.precio;
   total.innerHTML = `<h3 class="px-4 border-top border-3 py-4"> TOTAL: <strong>US$${carrito.total}</strong> </h3>`;
+  
+}
+
+function restarProducto(p) {
+  
+  let indexEncontrado = carrito.items.indexOf(p); // Extrae el index que ocupa el producto
+  
+  if (indexEncontrado >= 0 && p.cantidad > 0) {
+    p.cantidad--;
+    lista.children[indexEncontrado].innerHTML = `<div class="p-2 d-flex align-items-center">
+      <h4 class="px-4">${p.nombre}</h4>
+      <div class="px-4 border-start border-3 d-flex align-items-center"><strong>US$${p.precio}</strong></div> 
+      <div class="px-4 border-start border-3"><strong>Cant:</strong>${p.cantidad}</div>
+    </div>`;
+
+    carrito.total = carrito.total - p.precio;
+    total.innerHTML = `<h3 class="px-4 border-top border-3 py-4"> TOTAL: <strong>US$${carrito.total}</strong> </h3>`;
+  }
+
+  if (p.cantidad == 0){
+    lista.children[indexEncontrado].innerHTML = ``
+    carrito.items.splice(indexEncontrado);
+    console.log(carrito.items);
+  }
+
+  if (carrito.total == 0){
+    total.innerHTML = ``
+  }
   
 }
