91bf8a1bfa62f54e027c8bf108d3b1a4f1705a80
Emanuel Villegas <fevillegas.dev@gmail.com>
2025-06-27 15:56:16 -0300

    Traje todas las imagenes con su id

--- album.js
+++ album.js
@@ -1,6 +1,10 @@
 const imgAlbumMain = document.getElementById("img-al-main")
 const imgAlbum01 = document.getElementById("img-album-01")
 const imgAlbum02 = document.getElementById("img-album-02")
+const imgAlbum03 = document.getElementById("img-album-03")
+const imgAlbum04 = document.getElementById("img-album-04")
+const imgAlbum05 = document.getElementById("img-album-05")
+const imgAlbum06 = document.getElementById("img-album-06")
 
 imgAlbum01.onclick = function(){
     mostrarImg(imgAlbum01)
@@ -8,6 +12,22 @@ imgAlbum01.onclick = function(){
 
 imgAlbum02.onclick = function(){
     mostrarImg(imgAlbum02)
+}
+
+imgAlbum03.onclick = function(){
+    mostrarImg(imgAlbum03)
+}
+
+imgAlbum04.onclick = function(){
+    mostrarImg(imgAlbum04)
+}
+
+imgAlbum05.onclick = function(){
+    mostrarImg(imgAlbum05)
+}
+
+imgAlbum06.onclick = function(){
+    mostrarImg(imgAlbum06)
 }
 
 function mostrarImg (a){
