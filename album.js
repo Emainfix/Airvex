3fecb65b6cad537570a16a9cff88947ea109ad63
Emanuel Villegas <fevillegas.dev@gmail.com>
2025-06-29 16:15:29 -0300

    Eliminé la img que se mostraba por defecto

--- album.js
+++ album.js
@@ -1,35 +1,11 @@
 const imgAlbumMain = document.getElementById("img-al-main")
-const imgAlbum01 = document.getElementById("img-album-01")
-const imgAlbum02 = document.getElementById("img-album-02")
-const imgAlbum03 = document.getElementById("img-album-03")
-const imgAlbum04 = document.getElementById("img-album-04")
-const imgAlbum05 = document.getElementById("img-album-05")
-const imgAlbum06 = document.getElementById("img-album-06")
 
-imgAlbumMain.innerHTML = `<img src="${imgAlbum01.src}" class="img-main-album " alt="Imagen del álbum">`; // La galeria tiene por defecto la primer imagen
+const imagenes = document.querySelectorAll("#album-imgs img"); // Selecciono cada elemento img dentro de mi album
 
-imgAlbum01.onclick = function(){
-    mostrarImg(imgAlbum01)
-}
-
-imgAlbum02.onclick = function(){
-    mostrarImg(imgAlbum02)
-}
-
-imgAlbum03.onclick = function(){
-    mostrarImg(imgAlbum03)
-}
-
-imgAlbum04.onclick = function(){
-    mostrarImg(imgAlbum04)
-}
-
-imgAlbum05.onclick = function(){
-    mostrarImg(imgAlbum05)
-}
-
-imgAlbum06.onclick = function(){
-    mostrarImg(imgAlbum06)
-}
+imagenes.forEach(function(im){ // Recorro cada elemento img
+    im.addEventListener("click", () => {
+    mostrarImg(im)
+})
+})
 
 const mostrarImg = (a) => imgAlbumMain.innerHTML = `<img src="${a.src}" class="img-main-album " alt="Imagen del álbum">`; // Cuando se selecciona una miniatura la convierte a la img principal
