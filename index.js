517d2bea09a783eaf7a19033337233f2f6550a69
Emanuel Villegas <fevillegas.dev@gmail.com>
2025-06-29 18:43:23 -0300

    Mediante la misma tecnica de los botones agregar hice los eliminar

--- index.js
+++ index.js
@@ -31,81 +31,22 @@ botonesAgregar.forEach(b => { // Reviso uno a uno los elementos con la clase btn
   })
 })
 
+const botonesEliminar = document.querySelectorAll(".btn-eliminar")
+
+botonesEliminar.forEach(b => {
+  b.addEventListener("click", () =>{
+    const id = b.dataset.id;
+    restarProducto(productos[id]);
+  })
+})
+
 
 const lista = document.getElementById("lista-carrito");
 
 const total = document.getElementById("total");
 
 const tituloCarrito = document.getElementById("titulo-carrito");
-/*
-//Boton aspiradora1
-const aspiradora1 = new Producto(1, 0, "S10 Plus", 200);
 
-const aspiradora1btn = document.getElementById("boton-aspiradora1");
-const aspiradora1Eliminar = document.getElementById("eliminar-aspiradora1")
-
-aspiradora1btn.onclick = function () {
-  addProducto(aspiradora1);
-};
-aspiradora1Eliminar.onclick = function () {
-  if (aspiradora1.cantidad > 0){
-    restarProducto(aspiradora1);
-  }
-  
-};
-
-//Boton aspiradora2
-const aspiradora2 = new Producto(2, 0, "ROPVACNIC", 150);
-
-const aspiradora2btn = document.getElementById("boton-aspiradora2");
-const aspiradora2Eliminar = document.getElementById("eliminar-aspiradora2")
-
-aspiradora2btn.onclick = function () {
-  addProducto(aspiradora2);
-};
-aspiradora2Eliminar.onclick = function () {
-  restarProducto(aspiradora2);
-};
-
-//Boton aspiradora3
-const aspiradora3 = new Producto(3, 0, "A12 Pro", 180);
-
-const aspiradora3btn = document.getElementById("boton-aspiradora3");
-const aspiradora3Eliminar = document.getElementById("eliminar-aspiradora3")
-
-aspiradora3btn.onclick = function () {
-  addProducto(aspiradora3);
-};
-aspiradora3Eliminar.onclick = function () {
-  restarProducto(aspiradora3);
-};
-
-//Boton aspiradora4
-const aspiradora4 = new Producto(4, 0, "BL-20 Lidar", 350);
-
-const aspiradora4btn = document.getElementById("boton-aspiradora4");
-const aspiradora4Eliminar = document.getElementById("eliminar-aspiradora4")
-
-aspiradora4btn.onclick = function () {
-  addProducto(aspiradora4);
-};
-aspiradora4Eliminar.onclick = function () {
-  restarProducto(aspiradora4);
-};
-
-//Boton aspiradora5
-const aspiradora5 = new Producto(5, 0, "AI Ultra", 182);
-
-const aspiradora5btn = document.getElementById("boton-aspiradora5");
-const aspiradora5Eliminar = document.getElementById("eliminar-aspiradora5")
-
-aspiradora5btn.onclick = function () {
-  addProducto(aspiradora5);
-};
-aspiradora5Eliminar.onclick = function () {
-  restarProducto(aspiradora5);
-};
-*/
 const finalizarCompra = document.getElementById("btn-fin-compra")
 finalizarCompra.onclick = function()
 {
